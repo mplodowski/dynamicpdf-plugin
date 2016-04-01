cce70e95cef9489837ec516c9c980c2902640798
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2016-04-01 10:21:49 +0200

    Add cs_CZ locale

---
+++ lang/cs/lang.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'plugin'       => [
+        'name'        => 'Dynamické PDF',
+        'description' => 'Generování dynamických a upravitelných PDF.',
+    ],
+    'pdftemplates' => [
+        'label'                  => 'PDF Šablony',
+        'code'                   => 'Kód',
+        'title'                  => 'Nadpis',
+        'description'            => 'Popis',
+        'layout'                 => 'Layout',
+        'empty_option'           => '-- Bez šablony --',
+        'code_comment'           => 'Unikátní kód patřící pouze této šabloně',
+        'content_html'           => 'HTML',
+        'content_css'            => 'CSS',
+        'name'                   => 'Jméno',
+        'return_message'         => 'Zpět na seznam šablon',
+        'new_template'           => 'Nová šablona',
+        'new_layout'             => 'Nový layout',
+        'templates'              => 'Šablony',
+        'layouts'                => 'Layouty',
+        'delete_alert'           => 'Opravdu chcete smazat tuto PDF šablonu?',
+        'background_img'         => 'Obrázek pozadí',
+        'preview'                => 'Náhled',
+        'created_at'             => 'Vytvořeno v',
+        'updated_at'             => 'Upraveno v',
+        'background_img_comment' => 'Pro správné zobrazení použijte obrázek s 96 DPI'
+    ],
+    'pdftemplate'  => [
+        'menu_label'      => 'PDF Šablony',
+        'create_template' => 'Vytvoření PDF šablony',
+        'edit_template'   => 'Úprava PDF šablony',
+    ],
+    'pdflayouts'   => [
+        'label'          => 'PDF Layouty',
+        'return_message' => 'Zpět na seznam PDF layoutů',
+        'delete_alert'   => 'Opravdu chcete smazat tento PDF layout?',
+    ],
+    'pdflayout'    => [
+        'menu_label'    => 'PDF Layouty',
+        'create_layout' => 'Vytvoření PDF layoutu',
+        'edit_layout'   => 'Úprava PDF layoutu',
+    ],
+    'settings'     => [
+        'description' => 'Správa PDF šablon a layoutů.',
+    ],
+    'permissions'  => [
+        'label' => 'Správa PDF šablon',
+        'tab'   => 'PDF',
+    ],
+];
